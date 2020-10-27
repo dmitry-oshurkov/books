fed553afe5aca018793296b6ae598f31a3a68229
Dmitry Oshurkov <dm.oshurkov@prof-itgroup.ru>
2020-10-27 17:08:49 +0300

    + feed xml attributes

--- src/main/kotlin/name/oshurkov/books/catalog/CatalogModel.kt
+++ src/main/kotlin/name/oshurkov/books/catalog/CatalogModel.kt
@@ -4,6 +4,15 @@ import com.fasterxml.jackson.dataformat.xml.annotation.*
 
 @JacksonXmlRootElement(localName = "feed")
 data class Feed(
+
+    @JacksonXmlProperty(isAttribute = true, localName = "xmlns:dcterms") val dcterms: String = "http://purl.org/dc/terms/",
+    @JacksonXmlProperty(isAttribute = true, localName = "xmlns:thr") val thr: String = "http://purl.org/syndication/thread/1.0",
+    @JacksonXmlProperty(isAttribute = true, localName = "xmlns:opds") val opds: String = "http://opds-spec.org/2010/catalog",
+    @JacksonXmlProperty(isAttribute = true, localName = "xmlns:opensearch") val opensearch: String = "http://a9.com/-/spec/opensearch/1.1/",
+    @JacksonXmlProperty(isAttribute = true, localName = "xmlns:xsi") val xsi: String = "http://www.w3.org/2001/XMLSchema-instance",
+    @JacksonXmlProperty(isAttribute = true) val xmlns: String = "http://www.w3.org/2005/Atom",
+    @JacksonXmlProperty(isAttribute = true, localName = "xml:lang") val lang: String = "en",
+
     val id: String?,
     val title: String?,
     val updated: String?,
