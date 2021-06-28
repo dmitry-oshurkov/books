a5e0706443b00f9cc9a53208d2c51d025498eabc
Dmitry Oshurkov <dm.oshurkov@prof-itgroup.ru>
2021-06-28 18:48:18 +0300

    override equals, hashCode for EntityBase

--- src/main/kotlin/name/oshurkov/books/api/EntityBase.kt
+++ src/main/kotlin/name/oshurkov/books/api/EntityBase.kt
@@ -1,6 +1,7 @@
 package name.oshurkov.books.api
 
 import org.hibernate.annotations.*
+import org.springframework.data.util.*
 import java.util.*
 import javax.persistence.*
 import javax.persistence.GenerationType.*
@@ -13,4 +14,16 @@ abstract class EntityBase(
     @UpdateTimestamp
     @Column(nullable = false)
     val updated: Date = Date(0),
-)
+) {
+    override fun equals(other: Any?) = when {
+        other == null -> false
+        this === other -> true
+        javaClass != ProxyUtils.getUserClass(other) -> false
+        else -> {
+            other as EntityBase
+            id == other.id
+        }
+    }
+
+    override fun hashCode() = 42
+}
