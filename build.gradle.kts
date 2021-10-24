f471d1a1deaebcfd8f21c6e985915de507bbae1a
Dmitry Oshurkov <dmitry@oshurkov.name>
2021-10-24 12:44:06 +0300

    libs update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,12 @@ import org.gradle.api.JavaVersion.*
 import java.time.*
 
 plugins {
-    kotlin("jvm") version "1.5.20"
-    kotlin("plugin.spring") version "1.5.20"
-    kotlin("plugin.jpa") version "1.5.20"
-    id("org.springframework.boot") version "2.5.2"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
+    kotlin("plugin.jpa") version "1.5.31"
+    id("org.springframework.boot") version "2.5.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
     id("com.github.gmazzo.buildconfig") version "3.0.1"
 }
 
@@ -16,7 +16,7 @@ val ktorVersion: String by rootProject
 val buildNumber = (LocalDate.now().dayOfYear - 1) * 1440 + LocalTime.now().toSecondOfDay().div(60) // minute of year
 
 group = "name.oshurkov"
-version = "21.2.$buildNumber"
+version = "21.3.$buildNumber"
 java.sourceCompatibility = VERSION_11
 
 configurations {
@@ -32,17 +32,17 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.5.2-native-mt")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:2.5.1")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.1.5")
+    implementation("nz.net.ultraq.thymeleaf:thymeleaf-layout-dialect:3.0.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
-    implementation("org.apache.commons:commons-compress:1.20")
+    implementation("org.apache.commons:commons-compress:1.21")
     implementation("com.positiondev.epublib:epublib-core:3.1") {
         exclude(group = "org.slf4j")
         exclude(group = "xmlpull")
@@ -51,8 +51,8 @@ dependencies {
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.tukaani:xz:1.8") // for commons-compress
+    runtimeOnly("org.postgresql:postgresql:42.3.0")
+    runtimeOnly("org.tukaani:xz:1.9") // for commons-compress
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") { exclude(group = "org.junit.vintage", module = "junit-vintage-engine") }
@@ -71,7 +71,7 @@ tasks {
     compileKotlin {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "11"
+            jvmTarget = java.sourceCompatibility.majorVersion
         }
         dependsOn(ktlintFormat)
     }
