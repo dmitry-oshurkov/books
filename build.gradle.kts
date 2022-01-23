7bb9e08427880b1fea843c0326ba5ee5d86426d3
Dmitry Oshurkov <dmitry@oshurkov.name>
2022-01-23 19:46:39 +0300

    + reactor-core

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("io.projectreactor:reactor-core:3.4.11")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:$jacksonVersion")
     implementation("org.apache.commons:commons-compress:1.21")
--- src/main/kotlin/name/oshurkov/books/Common.kt
+++ src/main/kotlin/name/oshurkov/books/Common.kt
@@ -1,6 +1,7 @@
 package name.oshurkov.books
 
 import kotlinx.coroutines.*
+import org.slf4j.*
 import java.io.*
 import java.nio.*
 import java.nio.charset.*
@@ -8,17 +9,23 @@ import java.security.*
 import java.util.*
 import java.util.zip.*
 
+
 infix fun <A, B, C> Pair<A, B>.and(that: C) = Triple(this.first, this.second, that)
+
 
 fun <A, B> List<A>.pmap(f: suspend (A) -> B) = runBlocking {
     map { async { f(it) } }.awaitAll()
 }
+
 
 fun uuid(bytes: ByteArray) = run {
     val digest = MessageDigest.getInstance("MD5").digest(bytes)
     val bb = ByteBuffer.wrap(digest)
     UUID(bb.long, bb.long)
 }
+
+fun uuid(value: String) = UUID.fromString(value)!!
+
 
 fun unzipFile(file: File) =
     runCatching { ZipFile(file) }
@@ -27,3 +34,11 @@ fun unzipFile(file: File) =
             val entry = it.entries().toList().first()
             it.getInputStream(entry).use { stream -> stream.readAllBytes() }
         }!!
+
+
+fun Logger.debug(e: Throwable) = debug(e.message, e)
+fun Logger.info(e: Throwable) = info(e.message, e)
+fun Logger.warn(e: Throwable) = warn(e.message, e)
+fun Logger.error(e: Throwable) = error(e.message, e)
+
+fun logger() = lazy { LoggerFactory.getLogger(Thread.currentThread().stackTrace[4].className) }
